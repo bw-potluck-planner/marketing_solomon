76ef033bca6671899c563884291053d5fe30b620
zempo <zelenkosolomon@gmail.com>
2020-09-22 11:12:05 -0700

    add mobile nav links

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,9 @@
-console.log("hello");
+const burgerMenu = document.querySelector(".burger-menu");
+
+burgerMenu.addEventListener("click", (e) => {
+  if (burgerMenu.classList[1] === "burger-active") {
+    burgerMenu.classList.remove("burger-active");
+  } else {
+    burgerMenu.classList.add("burger-active");
+  }
+});
